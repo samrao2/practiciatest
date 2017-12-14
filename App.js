13ce77b712bb48afe735074b04cb32c32804e8c9
samrao1 <samrao1973@gmail.com>
2017-12-14 06:52:16 -0500

    next push

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import { Button } from 'react-native-elements';
+import {
+  Button,
+  FormLabel,
+  FormInput,
+  FormValidationMessage,
+  Divider } from 'react-native-elements';
 
 //import { Button } from './src/components/Button';
 //import { CardSection } from './src/components/CardSection';
@@ -9,36 +14,96 @@ import { Button } from 'react-native-elements';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: 'WELCOME!'
+    title: 'PRACTICIA'
   };
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        <Image source={require('./practicialogo.PNG')} />
-        <Text>Hello, Chat App!</Text>
+<View style={{ backgroundColor: 'white', flex: 1 }}>
+<Divider style={{ height: 100, backgroundColor: 'white' }} />
+
+<Text>Sign up as a...</Text>
+<Divider style={{ height: 200, backgroundColor: 'white' }} />
+
 
         <Button
         raised
         backgroundColor="#3399ff"
-        borderRadius="20"
-          onPress={() => navigate('Chat', { user: 'Lucy' })}
-          title="CHAT WITH LUCY"
+        borderRadius='20'
+          onPress={() => navigate('Teacher')}
+          title="TEACHER"
+        />
+        <Divider style={{ height: 15, backgroundColor: 'white' }} />
+
+        <Button
+        raised
+        backgroundColor="green"
+        borderRadius='20'
+          onPress={() => navigate('Parent')}
+          title="PARENT"
+        />
+        <Divider style={{ height: 15, backgroundColor: 'white' }} />
+
+        <Button
+        raised
+        backgroundColor="brown"
+        borderRadius='20'
+          onPress={() => navigate('Student')}
+          title="ADULT STUDENT (18+)"
         />
       </View>
     );
   }
 }
 
-class ChatScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: `Chat with ${navigation.state.params.user}`,
-  });
+class TeacherSignUp extends React.Component {
+  static navigationOptions = {
+    title: 'TEACHER SIGN UP',
+  };
   render() {
-    const { params } = this.props.navigation.state;
     return (
-      <View>
-        <Text>Chat with {params.user}</Text>
+      <View style={{ backgroundColor: 'white', flex: 1 }}>
+        <Text>Sign Up</Text>
+        <FormLabel>Email</FormLabel>
+        <FormInput />
+        <FormLabel>Password</FormLabel>
+        <FormInput />
+        <FormLabel>First Name</FormLabel>
+        <FormInput />
+        <FormLabel>LastNme</FormLabel>
+        <FormInput />
+        <Divider style={{ height: 10, backgroundColor: 'white' }} />
+        <Button
+        raised
+        backgroundColor="brown"
+        borderRadius='0'
+          // onPress={() => navigate()}
+          title="SUBMIT"
+        />
+      </View>
+    );
+  }
+}
+class ParentSignUp extends React.Component {
+  static navigationOptions = {
+    title: 'PARENT SIGN UP',
+  };
+  render() {
+    return (
+      <View style={{ backgroundColor: 'white', flex: 1 }}>
+        <Text>Sign Up</Text>
+      </View>
+    );
+  }
+}
+class StudentSignUp extends React.Component {
+  static navigationOptions = {
+    title: 'ADULT STUDENT SIGN UP',
+  };
+  render() {
+    return (
+      <View style={{ backgroundColor: 'white', flex: 1 }}>
+        <Text>Sign Up</Text>
       </View>
     );
   }
@@ -47,8 +112,9 @@ class ChatScreen extends React.Component {
 
 const SimpleApp = StackNavigator({
   Home: { screen: HomeScreen },
-  Chat: { screen: ChatScreen }
-
+  Teacher: { screen: TeacherSignUp },
+  Parent: { screen: ParentSignUp },
+  Student: { screen: StudentSignUp }
 });
 
 export default class App extends React.Component {
